6589a2a1017467155f6fa65b30593b7a1b4df336
Roman Korostinskiy <70313618+c71n93@users.noreply.github.com>
2025-11-15 18:05:04 +0300

    Initial main menu

--- app/src/main/java/com/c71n93/hat/MainActivity.java
+++ app/src/main/java/com/c71n93/hat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.c71n93.hat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button newGameButton = findViewById(R.id.button_new_game);
+        newGameButton.setOnClickListener(view -> startActivity(new Intent(this, GameSettingsActivity.class)));
     }
 }
